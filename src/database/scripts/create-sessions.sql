9a1609ac917258514d0e075f7d989dab585b1592
Pedro Lucas <pedrolucaspina22@gmail.com>
2021-10-25 15:05:39 -0300

    feat: add database's dump

--- src/database/scripts/create-sessions.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE "users" (
-  "id" SERIAL,
-  "userId" INTEGER,
-  "token" TEXT NOT NULL
-);
--- src/database/scripts/create-users.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE "transactions" (
-  "id" SERIAL,
-  "userId" INTEGER,
-  "value" NUMERIC,
-  "description" TEXT NOT NULL,
-  "password" TEXT NOT NULL
-);
